ec025cdb10abd8f5870df85ba28148e469d5ddd3
Olalekan Babawale <padinality@yahoo.com>
2019-11-15 14:10:17 +0100

    Add template and form

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/flaskmovie'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 @app.route('/')
 def index():
-    return "hello flask"
+    return render_template("index.html")
+
+@app.route("/post_user", methods=['POST'])
+def post_user():
+	user = User(request.form['username'], request.form['email'])
+	db.session.add(user)
+	db.session.commit()
+	return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
